871ff00d0d1cf7b124019886fbf4468e2ca58fc5
michaelpopedeveloper <michaelpopedeveloper@gmail.com>
2018-11-20 14:34:58 -0800

    initial commit

---
+++ cs_datastructures_algorithms/LinkedListNode.cs
@@ -0,0 +1,13 @@
+﻿namespace LinkedList
+{
+	public class LinkedListNode<T>
+	{
+		public LinkedListNode(T value)
+		{
+			Value = value;
+		}
+		public T Value { get; set;}
+
+		public LinkedListNode<T> Next { get; set;}
+	}
+}
